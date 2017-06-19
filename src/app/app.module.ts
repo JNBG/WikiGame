33e9aefaabaaf5b58e98b99eea7c9aab51384234
PhKi <Philipp.Kirste@gmx.de>
2017-06-20 01:01:01 +0200

    Added first Components (header, page-content, game, leaderboard)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PageContentComponent } from './page-content/page-content.component';
+import { LeaderboardComponent } from './page-content/leaderboard/leaderboard.component';
+import { GameComponent } from './page-content/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageContentComponent,
+    LeaderboardComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
